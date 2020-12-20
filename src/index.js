98a3b0f0bbc8bb8953ebc2b0bf264ca5b6622bf3
LenaMac <lenapronina1@LenaMacs-MacBook-Pro.local>
2020-12-20 21:19:17 +0300

    feat: add expand claimList funtion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import './styles/index.css';
-import { animateTicker } from './utils/utils.js'
+import { animateTicker } from './utils/utils.js';
+import claimList from './datasets/claims.js';
+
+
+
+const claimContainer = document.querySelector('.claim-list');
+console.log(claimContainer)
+
+function renderClaim (claimArray, container){
+  claimArray.forEach( item => {
+    const claimElement = document.querySelector('.claim-template').content.cloneNode(true);
+    const claimImage = claimElement.querySelector('.claim__id');
+    const claimHeader = claimElement.querySelector('.claim__cathegory');
+    const claimPreview = claimElement.querySelector('.claim__verse');
+    
+    claimImage.src = item.image;
+    claimHeader.textContent = item.cathegory;
+    claimPreview.textContent = item.preview;
+    
+    container.append(claimElement);
+  })
+}
+
+renderClaim(claimList.slice(0,3), claimContainer)
+
+const moreClaimsButton = document.querySelector('.popular__button');
+moreClaimsButton.addEventListener('click', ()=>{
+  renderClaim(claimList.slice(3,6), claimContainer)
+})
 
 // пока так сделала прокрутку по клику, потом может перепишем на что-то получше
 const arrowBottom = document.querySelector('.hello__arrow-bottom');
