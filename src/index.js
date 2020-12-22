d32b9a50d2afb832aa8a0e378637b31b955f6fc5
Ingrida <ingrida.surname@gmail.com>
2020-12-22 17:59:28 +0300

    ref: code style

--- src/index.js
+++ src/index.js
@@ -15,41 +15,41 @@ const claimContainer = document.querySelector('.claim-list');
 const popularText = document.querySelector('.popular__more');
 
 
-function renderClaim (claimArray, container){
-  claimArray.forEach( item => {
+function renderClaim(claimArray, container) {
+  claimArray.forEach(item => {
     const claimElement = document.querySelector('.claim-template').content.cloneNode(true);
     const claimImage = claimElement.querySelector('.claim__id');
     const claimHeader = claimElement.querySelector('.claim__cathegory');
     const claimPreview = claimElement.querySelector('.claim__verse');
     const claimBlock = claimElement.querySelector('.claim');
     const author = claimElement.querySelector('.claim__author');
-    
+
     claimImage.src = item.image;
     claimHeader.textContent = item.cathegory;
     claimPreview.textContent = item.preview;
 
     let claimClicked = false;
-    
-    claimBlock.addEventListener('click', ()=>{
-      if(claimClicked === false){
+
+    claimBlock.addEventListener('click', () => {
+      if (claimClicked === false) {
         claimPreview.textContent = item.text;
         claimClicked = true;
-        author.textContent =item.author
+        author.textContent = item.author
       } else {
         claimPreview.textContent = item.preview;
         claimClicked = false;
-        author.textContent =''
-      } 
+        author.textContent = ''
+      }
     })
     container.append(claimElement);
   })
 }
 
-renderClaim(claimList.slice(0,3), claimContainer)
+renderClaim(claimList.slice(0, 3), claimContainer)
 
 const moreClaimsButton = document.querySelector('.popular__button');
-moreClaimsButton.addEventListener('click', ()=>{
-  renderClaim(claimList.slice(3,6), claimContainer);
+moreClaimsButton.addEventListener('click', () => {
+  renderClaim(claimList.slice(3, 6), claimContainer);
   popularText.textContent = "Чуть позже добавим еще новости!";
   moreClaimsButton.style.display = 'none';
 })
@@ -64,7 +64,7 @@ const tickerLine = tickerContainer.querySelector('.ticker__line');
 
 const tickerArray = document.querySelectorAll('.ticker')
 
-tickerArray.forEach( item => {
+tickerArray.forEach(item => {
   animateTicker(item, '.ticker__line', 10000);
 });
 
@@ -88,12 +88,12 @@ goBackToCategoriesButton.addEventListener('click', () => {
     elem.remove();
   });
   subcategoryPopup.classList.remove('popup_opened');
-    
+
 });
 
 goBackToSubcategoriesButton.addEventListener('click', () => {
-    resultPopup.classList.remove('popup_opened');
-    formValidator.resetValidation();
+  resultPopup.classList.remove('popup_opened');
+  formValidator.resetValidation();
 });
 
 const newsCards = Array.from(document.querySelectorAll('.card'));
@@ -106,15 +106,17 @@ newsCards.forEach((card) => {
   })
 })
 
-newsPopupButton.addEventListener('click', function() {
+newsPopupButton.addEventListener('click', function () {
   newsPopup.classList.remove('popup_opened');
 })
 
 //enable form validation
+
 const formValidator = new FormValidator();
 formValidator.enableValidation();
 
 //переключатель стихотворений
+
 const poemSwitchToPrevButton = document.querySelector('.popup__poem-button_prev');
 const poemSwitchToNextButton = document.querySelector('.popup__poem-button_next');
 
@@ -129,6 +131,7 @@ poemSwitchToPrevButton.addEventListener('click', (evt) => {
 });
 
 //категории
+
 const problemList = document.querySelector('.problems-list__wrapper');
 const subcategoriesList = document.querySelector('.problems-list__wrapper_popup');
 
@@ -137,8 +140,8 @@ const createSubcategoryPopup = (subcategories, categoryName) => {
     const subcategoryCard = new InitialSubcategories(subcategory.name, subcategory.poems, openResultPopup);
     subcategoriesList.append(subcategoryCard.createElement());
   });
-    subcategoryPopupHeading.textContent = categoryName;
-    subcategoryPopup.classList.add('popup_opened');
+  subcategoryPopupHeading.textContent = categoryName;
+  subcategoryPopup.classList.add('popup_opened');
 };
 
 const openResultPopup = (poems) => {
